daa22b4356428ffa2287b9121dd73165cfc94c89
limkeunhyeok <gorloom6425@naver.com>
2019-03-09 23:33:42 +0900

    알고리즘 124나라

---
+++ temp.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = ''
+    arr = ['4','1','2']
+    while n > 0:
+        x = n % 3
+        n = n // 3
+        if x == 0:
+            n -= 1
+        answer += arr[x]
+    return answer[::-1]
+print(solution(5))
