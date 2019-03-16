cac9e637ecc0e60079fc2ab49839b1a05c27a833
limkeunhyeok <gorloom6425@naver.com>
2019-03-16 15:50:27 +0900

    알고리즘 같은 숫자는 싫어

--- temp.py
+++ temp.py
@@ -1,13 +1,9 @@
-def solution(citations):
-    answer = 0
-    count = max(citations)
-    for i in range(max(citations), 0, -1)
-        for j in citations:
-            if j >= count:
-                answer += 1
-            if count == answer:
-            return answer
-        else:
-            count -= 1
-            answer = 0
-# 테스트 16 실패(시간초과)
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    count = 0
+    for i in arr:
+        if i != answer[count]:
+            answer.append(i)
+            count += 1
+    return answer
