3fa5558f1a75d865e59e13106a216ddc51d50336
limkeunhyeok <gorloom6425@naver.com>
2019-03-16 16:53:02 +0900

    알고리즘 문자열 내림차순으로 배치하기

--- temp.py
+++ temp.py
@@ -1,13 +1,9 @@
 def solution(s):
-    answer = True
-    countp = 0
-    county = 0
-    for i in s:
-        if i == 'p' or i == 'P':
-            countp += 1
-        if i == 'y' or i == 'Y':
-            county += 1
-    if countp == county or (countp == county == 0):
-        return True
-    else:
-        return False
+    answer = ''
+    text = sorted(s)
+    for i in text[::-1]:
+        answer += i
+    return answer
+
+s = 'bcdZefg'
+print(solution(s))
