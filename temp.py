f80f883371ae2c52a0b717120be880ac1d7c2437
limkeunhyeok <gorloom6425@naver.com>
2019-03-16 17:08:40 +0900

    알고리즘 시저암호

--- temp.py
+++ temp.py
@@ -1,9 +1,19 @@
-def solution(s):
+import string
+
+def solution(s, n):
     answer = ''
-    text = sorted(s)
-    for i in text[::-1]:
-        answer += i
+    s = list(s)
+    alpha_low = list(string.ascii_lowercase)
+    alpha_up = list(string.ascii_uppercase)
+
+    for i in range(len(s)):
+        if s[i].islower():
+            answer += alpha_low[(alpha_low.index(s[i]) + n) % 26]
+        if s[i].isupper():
+            answer += alpha_up[(alpha_up.index(s[i]) + n) % 26]
+        if s[i] == ' ':
+            answer += ' '
     return answer
 
-s = 'bcdZefg'
-print(solution(s))
+s = 'AB'
+print(solution(s,1))
