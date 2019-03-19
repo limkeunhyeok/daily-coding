d8b7b3bb1efaa7ac5d8e04253bb7b0216bc6e9af
limkeunhyeok <gorloom6425@naver.com>
2019-03-19 20:46:32 +0900

    알고리즘 핸드폰 번호 가리기

--- temp.py
+++ temp.py
@@ -1,11 +1,9 @@
-def solution(n):
-    answer = 0
-    while n != 0:
-        answer += n % 10
-        n //= 10
-
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
+def solution(phone_number):
+    answer = ''
+    cnt = len(phone_number) - 4
+    for i in range(len(phone_number)):
+        if i >= cnt:
+            answer += phone_number[i]
+        else:
+            answer += '*'
     return answer
