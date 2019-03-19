c7411bc561d3a62660f896b8321a5e8ac1e2dfdf
limkeunhyeok <gorloom6425@naver.com>
2019-03-19 20:15:40 +0900

    알고리즘 자릿수 더하기

--- temp.py
+++ temp.py
@@ -1,11 +1,11 @@
-def solution(s):
-    answer = ''
-    count = 1
-    for i in range(len(s)):
-        if s[i] != ' ' and count % 2 == 1:
-            answer += s[i].upper()
-            count += 1
-        else:
-            answer += s[i].lower()
-            count = 1
+def solution(n):
+    answer = 0
+    while n != 0:
+        answer += n % 10
+        n //= 10
+
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
     return answer
