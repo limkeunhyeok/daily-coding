37a8a45eb65f65512275739d06fd6397f687ea6b
limkeunhyeok <gorloom6425@naver.com>
2019-06-07 21:25:46 +0900

    알고리즘 소수찾기

--- 알고리즘/파이썬/temp.py
+++ 알고리즘/파이썬/temp.py
@@ -1,9 +1,23 @@
-def solution(d, budget):
-    answer = 0
-    d.sort()
-    for i in range(len(d)):
-        budget -= d[i]
-        if budget < 0:
-            break
-        answer += 1
-    return answer
+import itertools
+import math
+
+def isPrime(num):
+    if num <= 1:
+        return False
+    r = int(math.sqrt(num))
+    for i in range(2,r+1):
+        if num % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    answer = []
+    numbers = list(numbers)
+    r = len(numbers)
+    for i in range(r):
+        temp = list(map(''.join, itertools.permutations(numbers,r-i)))
+        for j in temp:
+            if isPrime(int(j)):
+                answer.append(int(j))
+    answer = list(set(answer))
+    return len(answer)
