0f9b8b7b3e532ea3c5e0bd05e88c03813308aced
LKH <gorloom6425@naver.com>
2019-09-02 16:53:10 +0900

    알고리즘 오픈 채팅방

--- temp.py
+++ temp.py
@@ -1,9 +1,27 @@
-def solution(phone_number):
-    answer = ''
-    cnt = len(phone_number) - 4
-    for i in range(len(phone_number)):
-        if i >= cnt:
-            answer += phone_number[i]
+def solution(record):
+    answer = []
+    commands = []
+    user_key = {} 
+    
+    # user_id와 nick 매핑
+    for i in range(len(record)):
+        commandline = record[i].split(' ')
+        commands.append(commandline[0] + ' ' + commandline[1])
+        if commandline[0] == "Leave":
+            continue
         else:
-            answer += '*'
+            user_key[commandline[1]] = commandline[2]
+    
+    # 메시지 출력        
+    for i in range(len(commands)):
+        command = commands[i].split(' ')
+        if command[0] == 'Enter':
+            answer.append('%s님이 들어왔습니다.' % user_key[command[1]])
+        elif command[0] == 'Leave':
+            answer.append('%s님이 나가셨습니다.' % user_key[command[1]])
+        else:
+            continue
     return answer
+
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+print(solution(record))
