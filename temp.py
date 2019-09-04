89993116c38ba890263df9395b7eda3f4ba21829
LKH <gorloom6425@naver.com>
2019-09-05 01:09:32 +0900

    알고리즘 후보키

--- temp.py
+++ temp.py
@@ -1,35 +1,107 @@
-def solution(N, stages):
-    user = len(stages) # 총 도전자 수
-    fail = [] # 실패율
-    answer = []
-    
-    # 실패율 체크
-    for i in range(N):
-        user_stage = 0 # 클리어하지 못한 도전자 수
-        for j in range(len(stages)):
-            if stages[j] == i + 1:
-                user_stage += 1
-        if user == 0:
-            fail.append(0)
-            continue
-        fail.append(user_stage / user)
-        user -= user_stage
+from itertools import combinations
 
-    # 스테이지와 실패율 매핑
-    dict = {}
-    for i in range(len(fail)):
-        dict[i+1] = fail[i]
+# 내가 작성한 코드, 18, 19, 20, 22, 25 테스트 케이스 실패
+class mysolution: 
     
-   # 스테이지 내림차순
-    while N != len(answer):
-        temp = 0
-        keys = list(dict.keys())
-        for i in keys:
-            if dict[i] > temp:
-               temp = dict[i]
-        for i in keys:
-            if dict[i] == temp:
-                answer.append(i)
-                del dict[i]
-                break
+    # 유일성 만족 체크
+    def isUnique(arr):
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                if arr[i] == arr[j]:
+                    return False
+        return True
+    
+    # 2차원 배열 column 가져오기
+    def getColumn(arr_2d, comb):
+        arr_1d = []
+        for i in range(len(arr_2d)):
+            temp = ''
+            for j in range(len(comb)):
+                temp += str(arr_2d[i][comb[j]])
+            arr_1d.append(temp)
+        return arr_1d
+    
+    # 최소성을 만족하는 key의 갯수
+    def getMin(keys):
+        arr = []
+        check = []
+        for i in range(len(keys)):
+            s = ''
+            for j in range(len(keys[i])):
+                s += str(keys[i][j])
+            arr.append(s)
+    
+        for i in range(len(arr)):
+            check.append(True)
+    
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                if arr[i] in arr[j]:
+                    check[j] = False
+    
+        cnt = 0
+        for i in check:
+            if i == True:
+                cnt += 1
+        return cnt
+                    
+    
+    def solution(relation):
+        element = 1
+        rows = len(relation[0])
+        keys = []
+        while element <= rows:
+            check_cols = list(combinations(range(rows),element))
+            for i in range(len(check_cols)):
+                cols = getColumn(relation,check_cols[i])
+                if isUnique(cols):
+                    keys.append(check_cols[i])
+            element += 1
+        cnt = getMin(keys)
+        return cnt
+
+
+# 다른 사람이 작성한 코드
+def other_solution(relation):
+    answer = 0
+    all = list()
+    uniqeIndex = []
+    
+    if len(relation) > 0:
+        cols = len(relation[0])
+        rows = len(relation)
+        
+        for i in range(1, cols + 1):
+            all.extend([set(k) for k in combinations([j for j in range(cols)], i)])
+
+        for comb in all:
+            vaildSet = set()
+            for row in range(rows):
+                temp = ''
+                for col in comb:
+                    temp += relation[row][col]
+                vaildSet.add(temp)
+
+            if len(vaildSet) == rows:
+                uniqeIndex.append(comb)
+
+    delSet = set()
+
+    for stdMinElem in uniqeIndex:
+        for idx, compMinElem in enumerate(uniqeIndex):
+            if stdMinElem.issubset(compMinElem) and stdMinElem != compMinElem:
+                delSet.add(uniqeIndex.index(compMinElem))
+    answer = len(uniqeIndex)-len(delSet)
     return answer
+
+
+# 예제 테스트
+relation = [["100","ryan","music","2"],
+            ["200","apeach","math","2"],
+            ["300","tube","computer","3"],
+            ["400","con","computer","4"],
+            ["500","muzi","music","3"],
+            ["600","apeach","music","2"]]
+
+print(mysolution.solution(relation))
+print(other_solution(relation))
