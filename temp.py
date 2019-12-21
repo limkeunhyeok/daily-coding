863f054b95d282a92f87608987570c261641d3cc
LKH <gorloom6425@naver.com>
2019-12-21 20:45:15 +0900

    알고리즘 짝지어 제거하기

--- temp.py
+++ temp.py
@@ -1,17 +1,14 @@
-def solution(board):
-    check = 0
-    for i in range(len(board)):
-        check += sum(board[i])
-    if check == 0:
-        return 0
-    elif check == 1:
+def solution(s):
+    temp = []
+    index = 0
+    while index != len(s):
+        temp.append(s[index])
+        index += 1
+        if len(temp) > 1:
+            if temp[-1] == temp[-2]:
+                temp.pop()
+                temp.pop()
+    if len(temp) == 0:
         return 1
     else:
-        temp = board
-        max_length = 0
-        for i in range(1, len(board)):
-            for j in range(len(1, board[i])):
-                if temp[i][j]:
-                    temp[i][j] += min(temp[i - 1][j], temp[i][j - 1], temp[i - 1][j - 1])
-                    max_length = max(max_length, temp[i][j])
-        return max_length ** 2
+        return 0
