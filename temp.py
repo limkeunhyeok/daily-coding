aab9cc24488c4afa849d25bd2750763a4c470d9a
LKH <gorloom6425@naver.com>
2019-12-26 21:15:08 +0900

    알고리즘 캐시

--- temp.py
+++ temp.py
@@ -1,4 +1,21 @@
-def solution(numbers):
-    temp = list(map(str, numbers))
-    temp.sort(key=lambda x:3*x, reverse = True)
-    return str(int(''.join(temp)))
+def solution(cacheSize, cities):
+    if cacheSize == 0:
+        return len(cities) * 5
+    cache = []
+    cities = [s.lower() for s in cities]
+    count = 0
+    
+    for city in cities:
+        if city in cache:
+            count += 1
+            cache.remove(city)
+            cache.append(city)
+            continue
+        else:
+            count += 5
+            if len(cache) == cacheSize:
+                cache.remove(cache[0])
+                cache.append(city)
+            else:
+                cache.append(city)
+    return count
