a13c6b1d9ec96d05a3f5f3a4769d97839c61c741
LKH <gorloom6425@naver.com>
2020-05-29 01:44:12 +0900

    알고리즘 3 X N 타일링

--- programmers/Level_4/3 x N 타일링/solution.py
+++ programmers/Level_4/3 x N 타일링/solution.py
@@ -1,8 +1,9 @@
 def solution(n):
-    dp = [3, 11]
+    dp = [1, 3]
+    if (n // 2) <= 1:
+        return dp[n // 2]
     
-    for i in range(2, (n // 2)):
-        temp = (dp[i - 1] + dp[i - 2]) % 1000000007
+    for i in range(2, (n // 2) + 1):
+        temp = (3 * dp[i - 1] + 2 * sum(dp[:i - 1])) % 1000000007
         dp.append(temp)
-        
     return dp[-1]
--- programmers/Level_4/3 x N 타일링/testCase.py
+++ programmers/Level_4/3 x N 타일링/testCase.py
@@ -6,5 +6,8 @@ class temp(unittest.TestCase):
         result = solution.solution(4)
         self.assertEqual(result, 11)
 
+    def test_case_2(self):
+        result = solution.solution(6)
+        self.assertEqual(result, 41)
 if __name__ == '__main__':
     unittest.main()
