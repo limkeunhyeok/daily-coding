deb68792dfedff80e46864a23eed480e45c0762e
LKH <gorloom6425@naver.com>
2020-06-09 22:30:29 +0900

    divide & conquer

---
+++ 알고리즘/divideConquer/quadtree.py
@@ -0,0 +1,31 @@
+"""
+출처: 알고스팟(https://www.algospot.com/judge/problem/read/QUADTREE)
+"""
+
+def solution(tree):
+    return reverse(iter(tree))
+
+def reverse(it):
+    current = next(it)
+    if current == 'w' or current == 'b':
+        return current
+
+    upperLeft = reverse(it)
+    upperRight = reverse(it)
+    lowerLeft = reverse(it)
+    lowerRight = reverse(it)
+
+    return 'x' + lowerLeft + lowerRight + upperLeft + upperRight
+
+
+
+def check(result, answer):
+    if result == answer:
+        return True
+    return False
+
+case = ['w', 'xbwwb', 'xbwxwbbwb', 'xxwwwbxwxwbbbwwxxxwwbbbwwwwbb']
+answer = ['w', 'xwbbw', 'xxbwwbbbw', 'xxwbxwwxbbwwbwbxwbwwxwwwxbbwb']
+
+for i in range(4):
+    print(check(solution(case[i]), answer[i]))
