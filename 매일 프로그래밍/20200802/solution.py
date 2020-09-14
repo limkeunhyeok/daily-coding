dc05021d804d59545a0aca779f2f9424a9f6c1e3
LKH <gorloom6425@naver.com>
2020-09-15 00:18:12 +0900

    매일 프로그래밍 8월 첫째주

---
+++ 매일 프로그래밍/20200802/solution.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+
+class Node:
+    def __init__(self, data):
+        self.data = data;
+        self.left = None
+        self.right = None
+        self.level = 1
+        
+class BST:
+    def __init__(self):
+        self.root = None
+        self.d = defaultdict(list)
+    
+    def insert(self, data):
+        newNode = Node(data)
+        
+        if self.root is None:
+            self.root = newNode
+            self.d[newNode.level].append(newNode.data)
+        else:
+            current = self.root
+            while True:
+                if data > current.data:
+                    if current.right is None:
+                        newNode.level += 1
+                        current.right = newNode
+                        self.d[newNode.level].append(newNode.data)
+                        break
+                    else:
+                        current = current.right
+                        newNode.level += 1
+                else:
+                    if current.left is None:
+                        newNode.level += 1
+                        current.left = newNode
+                        self.d[newNode.level].append(newNode.data)
+                        break
+                    else:
+                        current = current.left
+                        newNode.level += 1
+
+
+t = BST()
+t.insert(7)
+t.insert(5)
+t.insert(10)
+t.insert(3)
+t.insert(6)
+t.insert(8)
+t.insert(12)
+# 7
+# 5 10
+# 3 6 8 12
+
+d = t.d
+answer = []
+for key in d.keys():
+    if key % 2 == 1:
+        answer += d[key]
+    else:
+        answer += sorted(d[key], reverse = True)
+        
+print(answer)
