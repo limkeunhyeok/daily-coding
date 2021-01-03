b152bc5e8b9ee545cb7e788816538a8f3a10ef69
LKH <gorloom6425@naver.com>
2021-01-03 22:37:58 +0900

    백준 보물

---
+++ 백준/1026/solution.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+a = list(map(int, input().split(' ')))
+b = list(map(int, input().split(' ')))
+
+a = sorted(a)
+b = sorted(b, reverse=True)
+
+answer = 1
+for i in range(n):
+    answer += a[i] * b[i]
+print(answer)
