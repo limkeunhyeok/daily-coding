1edcde79e0b3a4ac95bb99ae5dc678a5abbb4795
Keunhyeok Lim <38815618+limkeunhyeok@users.noreply.github.com>
2021-03-15 18:43:18 +0900

    백준 1로 만들기

---
+++ 백준/1463/solution.py
@@ -0,0 +1,14 @@
+def solution(n):
+    dp = [0 for _ in range(n + 1)]
+    
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0 and dp[i] > dp[i // 2] + 1 :
+            dp[i] = dp[i // 2] + 1
+            
+        if i % 3 == 0 and dp[i] > dp[i // 3] + 1 :
+            dp[i] = dp[i // 3] + 1
+    return dp[n]
+
+n = int(input())
+print(solution(n))
