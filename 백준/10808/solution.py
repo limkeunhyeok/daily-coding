77779e92c09d8681e7627704bc77a3809990e73d
limkeunhyeok <gorloom6425@naver.com>
2021-11-29 01:50:53 +0900

    백준 알파벳 개수

---
+++ 백준/10808/solution.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    S = input()
+    dic = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
+    for c in S:
+        dic[c] += 1
+    
+    answer = list(dic.values())
+    for num in answer:
+        print(num, end = " ")
